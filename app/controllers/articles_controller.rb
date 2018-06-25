77685b0d5804a4bd8a5370bd5b5aef94cc6cde27
bobi <boban0092@gmail.com>
2018-06-25 22:40:16 +0200

    add pagination to usersand articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
     http_basic_authenticate_with name: "bobivankenobi", password: "bobicar92", except: [:index, :show]
     before_action :set_article, only: [:edit, :update, :show, :destroy]
 def index
-    @articles=Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 3)
 end
 
 
