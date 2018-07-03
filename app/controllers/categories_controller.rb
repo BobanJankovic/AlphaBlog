c423d239b9308b48e962119ccec4766984bd8cbc
bobi <boban0092@gmail.com>
2018-07-03 21:40:34 +0200

    Controller Tests

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,23 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+  def index
+    @categories=Category.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  private
+
+  def set_category
+    @category= Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
