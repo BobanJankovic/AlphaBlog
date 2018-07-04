48dc4b3c3087527e02ce5a3b4256fdc9c4529f10
bobi <boban0092@gmail.com>
2018-07-04 22:39:36 +0200

    invalid category submission Test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -11,10 +11,24 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     #create action
     assert_difference 'Category.count', 1 do
       post categories_path, params: {category: {name:"football"} }
-      #PUSI GA LINU 14
       follow_redirect!
     end
     assert_template 'categories/index'
     assert_match "football", response.body
+  end
+
+  test "invalid category submission results in failure" do
+    
+    get new_category_path
+    
+    assert_template 'categories/new'
+    #create action
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name:" "} }
+      
+    end
+    assert_template 'categories/new'
+    assert_select 'h2.card-title'
+    assert_select 'p.card-text'
   end
 end
