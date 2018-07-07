8cca1dd20f757a3a114f88f56d010cf917fb7039
bobi <boban0092@gmail.com>
2018-07-07 21:47:49 +0200

    Edit categories controller and button

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,7 +13,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    
+     
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -23,6 +23,20 @@ class CategoriesController < ApplicationController
       flash[:notice] = "Category was successfully created"
       redirect_to categories_path
     else render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+    
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
     end
   end
 
