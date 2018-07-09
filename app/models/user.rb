95c79085ebd765e4044ace236c4cd4ced62894ca
bobi <boban0092@gmail.com>
2018-07-09 23:28:18 +0200

    50char

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     #dependent if we destroy our user it will also destroy all articles that user has been created
     has_many :articles, dependent: :destroy
     before_save { self.email = email.downcase }
-    validates :username, presence:true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25}
+    validates :username, presence:true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 105}, uniqueness: { case_sensitive: false }, 
     #chek the validity of email format
